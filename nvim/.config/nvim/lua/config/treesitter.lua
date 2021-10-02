7b4667842e0d79cbe0aa0e3d1afd3f5c22e0e780
Shreeram Modi <67710369+Shrimpram@users.noreply.github.com>
2021-10-02 12:15:17 -0700

    NVIM: Add html to TS parsers

--- nvim/.config/nvim/lua/config/treesitter.lua
+++ nvim/.config/nvim/lua/config/treesitter.lua
@@ -1,13 +1,31 @@
 local ts_configs = require('nvim-treesitter.configs')
 ts_configs.setup {
-    ensure_installed = {
-        'bash', 'bibtex', 'c', 'c_sharp', 'comment', 'cpp', 'css', 'go',
-        'html', 'java', 'javascript', 'json', 'jsonc', 'latex', 'lua',
-        'nix', 'python', 'regex', 'rust', 'toml', 'yaml'
-    },
-    highlight = {
-        enable = true,
-        disable = { "html" },
-        use_languagetree = true,
-    }
+  ensure_installed = {
+    'bash',
+    'bibtex',
+    'c',
+    'c_sharp',
+    'cmake',
+    'comment',
+    'cpp',
+    'css',
+    'go',
+    'html',
+    'java',
+    'javascript',
+    'json',
+    'jsonc',
+    'latex',
+    'lua',
+    'nix',
+    'python',
+    'regex',
+    'rust',
+    'toml',
+    'yaml'
+  },
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  }
 }
