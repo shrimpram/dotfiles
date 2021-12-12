0df03bfed6c35394cb01d5ec7009abffb2810027
Shreeram Modi <67710369+Shrimpram@users.noreply.github.com>
2021-12-12 12:03:27 -0800

    NIX: Use stable nvim instead of head

--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -98,13 +98,6 @@
 
   ];
 
-  /* Use neovim from head
-     See https://github.com/nix-community/neovim-nightly-overlay/ */
-  nixpkgs.overlays = [
-    (import (builtins.fetchTarball {
-             url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
-             }))
-  ];
 
   programs.git = {
     enable = true;
