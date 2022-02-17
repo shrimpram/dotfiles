79ce106d5269679dba44f11b8a11ec3a485dbf2a
Shreeram Modi <67710369+Shrimpram@users.noreply.github.com>
2022-02-16 17:21:44 -0800

    NIX-BAT: Disable theme
    The Stella scheme is currently undergoing a rework, after which themes
    for other apps will become avaiable for use. Should be reverted or fixed
    when the rework is done/released.

--- nix/.config/nixpkgs/modules/bat/default.nix
+++ nix/.config/nixpkgs/modules/bat/default.nix
@@ -2,17 +2,10 @@
 {
   programs.bat = {
     enable = true;
-    themes = {
-      stella = builtins.readFile (builtins.fetchGit {
-        url = "https://github.com/stella-scheme/sublime";
-        ref = "main";
-      } + "/stella.tmTHeme");
-    };
     config = {
-      theme = "stella";
       style = "full";
-      italic-text="always";
-      wrap="never";
+      italic-text = "always";
+      wrap = "never";
     };
   };
 }
