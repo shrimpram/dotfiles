91eab458e4b98731535fcd5ee0d8a497a7a60367
Shreeram Modi <67710369+Shrimpram@users.noreply.github.com>
2022-03-11 08:33:54 -0800

    NIX: Add ~/go/bin to $PATH

--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -40,6 +40,7 @@
       "$HOME/Library/Python/3.9/bin"
       "$HOME/.gem/ruby/2.6.0/bin"
       "$HOME/.gem/ruby/2.7.0/bin"
+      "$HOME/go/bin"
     ];
     packages = with pkgs; [
         # Core
