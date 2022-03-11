6567deb7736b5f4627bb3db7aebf6284c572047d
Shreeram Modi <67710369+Shrimpram@users.noreply.github.com>
2022-03-11 08:36:01 -0800

    NVIM: Update nvim-tree setup function

--- nvim/.config/nvim/lua/config/nvim-tree.lua
+++ nvim/.config/nvim/lua/config/nvim-tree.lua
@@ -1,12 +1,14 @@
 local map = vim.api.nvim_set_keymap
 local let = vim.api.nvim_set_var
 
-let( 'nvim_tree_ignore', { '.git', '.cache' } ) -- empty by default
 let( 'nvim_tree_indent_markers', 1 ) -- 0 by default, this option shows indent markers when folders are open
 let( 'nvim_tree_add_trailing', 1 ) -- 0 by default, append a trailing slash to folder names
-let( 'nvim_tree_quit_on_open', 1 ) -- 0 by default, closes the tree when you open a file
 let( 'nvim_tree_special_files', { 'README.md', 'Makefile', 'MAKEFILE', 'LICENSE' } ) -- List of filenames that gets highlighted with NvimTreeSpecialFile
 
 require('nvim-tree').setup {
   auto_close = true,
+  ignore_ft_on_setup = { '.git', '.cache' },
+  open_file = {
+    quit_on_open = true,
+  },
 }
