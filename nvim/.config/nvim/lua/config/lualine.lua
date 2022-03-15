005c7e4ac1acd516bc938c1f91825c3f2f8259eb
Shreeram Modi <67710369+Shrimpram@users.noreply.github.com>
2022-03-15 10:07:51 -0700

    NVIM: Add spell and trailing components for lualine
    - Spell checks whether or not spellcheck is enabled and returns the
      spelllang
    - Trailing returns the line number of lines with trailing whitespace

--- nvim/.config/nvim/lua/config/lualine.lua
+++ nvim/.config/nvim/lua/config/lualine.lua
@@ -1,3 +1,20 @@
+local function spell()
+  if vim.o.spell then
+    return string.format('SPELL[%s]', string.upper(vim.o.spelllang))
+  else
+    return ''
+  end
+end
+
+local function trailing()
+  local line_nr = vim.fn.search([[\s$]], 'nw')
+  if line_nr == 0 then
+    return ''
+  end
+
+  return string.format('T:%d', line_nr)
+end
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
@@ -7,6 +24,7 @@ require('lualine').setup {
     always_divide_middle = true,
   },
   sections = {
+    lualine_a = { 'mode', spell },
     lualine_b = {
       {
         'diff',
@@ -14,5 +32,6 @@ require('lualine').setup {
         symbols = {added = '+', modified = '~', removed = '-'},
       }
     },
+    lualine_x = { trailing, 'encoding', 'fileformat', 'filetype' },
   },
 }
