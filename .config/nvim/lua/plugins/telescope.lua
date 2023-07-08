30b878406d7c6fbe226c0d97fb272070ccbc91b0
Shreeram Modi <shreerammodi10@gmail.com>
2023-07-07 21:01:49 -0700

    chore(nvim): deprecate hydra config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -20,6 +20,45 @@ return {
       require("telescope").load_extension("file_browser")
       require("telescope").load_extension("fzf")
     end,
+    keys = {
+      {
+        "<leader>tb",
+        function()
+          require("telescope.builtin").buffers()
+        end,
+        desc = "Telescope Buffers"
+      },
+      {
+        "<leader>tf",
+        function()
+          require("telescope.builtin").find_files()
+        end,
+        desc = "Telescope Find Files"
+      },
+      {
+        "<leader>tt",
+        function()
+          require("telescope.builtin").git_files()
+        end,
+        desc = "Telescope Git Files"
+      },
+      {
+        "<leader>tr",
+        mode = { "n" },
+        function()
+          require("telescope.builtin").live_grep()
+        end,
+        desc = "Telescope Grep"
+      },
+      {
+        "<leader>tr",
+        mode = { "x" },
+        function()
+          require("telescope.builtin").grep_string()
+        end,
+        desc = "Telescope Grep"
+      },
+    },
   },
 
 }
