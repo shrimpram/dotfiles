42b79d156570cc15fac7e55dde977aaa55dcc2af
Shreeram Modi <shreerammodi10@gmail.com>
2023-08-02 00:44:49 -0700

    feat(nvim): add neorg config

---
+++ .config/nvim/lua/plugins/neorg.lua
@@ -0,0 +1,24 @@
+return {
+
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    event = "VeryLazy",
+    opts = {
+      load = {
+        ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              media = "~/Documents/10-19 personal/13 media/11.01 media-list",
+            },
+          },
+        },
+      },
+    },
+  },
+}
