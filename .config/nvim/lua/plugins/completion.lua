52ac71b2a130266eec47cba86524e3d4b4e680ab
Shreeram Modi <shreerammodi10@gmail.com>
2023-09-07 00:47:11 -0400

    fix(nvim): trigger snippet expansion on <Tab>
    Previously a snippet would only be jumped through on <Tab>, or cmp would
    cycle through completion options. Now, a snippet is chosen on tab, which
    is intended. Completion options can still be cycled through using <C-n>
    or <C-p>.

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -17,4 +17,41 @@ return {
       require("shreeram.conf.cmp")
     end,
   },
+
+  {
+    "L3MON4D3/LuaSnip",
+    build = "make install_jsregexp",
+    config = function()
+      require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets/" })
+    end,
+    opts = {
+      history = true,
+      delete_check_events = "TextChanged",
+    },
+    keys = {
+      {
+        "<tab>",
+        function()
+          return require("luasnip").expand_or_jumpable(1) and "<Plug>luasnip-expand-or-jump" or "<tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = "i",
+      },
+      {
+        "<tab>",
+        function()
+          require("luasnip").jump(1)
+        end,
+        mode = "s",
+      },
+      {
+        "<s-tab>",
+        function()
+          require("luasnip").jump(-1)
+        end,
+        mode = { "i", "s" },
+      },
+    },
+  },
 }
