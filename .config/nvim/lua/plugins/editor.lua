c08f8587ecd2a92b8897b918b1b00fd1dbcd35ba
Shreeram Modi <shreerammodi10@gmail.com>
2023-09-19 01:17:05 -0400

    fix(nvim): use surround keybindings for sandwich
    The default vim sandwich keybindings override the "s" key. This is
    problematic because not only is it a default vim key but it is also used
    by motion plugins like sneak or leap, which I prioritize over sandwich.
    Sandwich provides a keybindings option[1] that loads vim-surround's default
    keybindings which are saner.
    
    Using lazy's init table is necessary to disable default keybindings
    because the sandwich_no_default_key_mappings check is run in the
    sandwich plugin file, which means the variable must be defined before
    the file is loaded.
    
    [1] https://github.com/machakann/vim-sandwich/wiki/Introduce-vim-surround-keymappings

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -33,7 +33,15 @@ return {
     },
   },
 
-  "machakann/vim-sandwich",
+  {
+    "machakann/vim-sandwich",
+    init = function()
+      vim.cmd([[let g:sandwich_no_default_key_mappings = 1]])
+    end,
+    config = function()
+      vim.cmd([[runtime macros/sandwich/keymap/surround.vim]])
+    end,
+  },
 
   "tpope/vim-speeddating",
 
