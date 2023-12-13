bd1bfe03f2e64138b31fcd84efeece30367f4f7b
Shreeram Modi <shreerammodi10@gmail.com>
2023-12-13 15:55:41 -0500

    chore(nvim): cleanup lsp config

--- .config/nvim/lua/shreeram/conf/lsp.lua
+++ .config/nvim/lua/shreeram/conf/lsp.lua
@@ -2,18 +2,21 @@ local lspconfig = require("lspconfig")
 
 require("mason").setup({
   ensure_installed = {
+    "bash_language_server",
     "prettier",
-    "stylua",
-    "latexindent",
+    "shellcheck",
+    "tailwindcss",
+    "texlab",
   },
 })
 
 require("mason-lspconfig").setup({
   ensure_installed = {
-    "lua_ls",
-    "texlab",
-    "tailwindcss",
     "html",
+    "jdtls",
+    "lua_ls",
+    "tailwindcss",
+    "texlab",
     "tsserver",
   },
 })
@@ -31,18 +34,8 @@ lspconfig.lua_ls.setup({
 lspconfig.tailwindcss.setup({})
 lspconfig.texlab.setup({})
 lspconfig.tsserver.setup({})
+lspconfig.jdtls.setup({})
 
 local on_attach = function(_, bufnr)
-  vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc")
-
-  -- vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  -- vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end,)
-  -- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  -- vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  -- vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  -- vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  -- vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  -- vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  -- vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  -- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "gk", "<cmd>Lspsaga hover_doc")
 end
