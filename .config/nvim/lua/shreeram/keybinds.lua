db08b52bc6543817c0a38ed8c2e130ef7b7d4475
Shreeram Modi <smodi@smodi.net>
2024-07-30 17:21:05 -0700

    nvim: Add useful yank and paste mappings

--- .config/nvim/lua/shreeram/keybinds.lua
+++ .config/nvim/lua/shreeram/keybinds.lua
@@ -33,3 +33,10 @@ vim.keymap.set("n", "<leader>cn", vim.cmd.cnext, { desc = "Go to next error" })
 vim.keymap.set("n", "<leader>cp", vim.cmd.cprev, { desc = "Go to prev error" })
 
 vim.keymap.set("i", "<C-l>", "<C-G>u<Esc>[s1z=`]a<C-G>u", { desc = "Correct spelling error" })
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [[<cmd>%y +<cr>]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
