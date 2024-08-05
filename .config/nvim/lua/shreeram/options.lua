4f77f2a43de107808f2b7702b5ed6eaa6aad1974
Shreeram Modi <smodi@smodi.net>
2024-08-04 18:20:03 -0700

    nvim: Revamp options

--- .config/nvim/lua/shreeram/options.lua
+++ .config/nvim/lua/shreeram/options.lua
@@ -1,25 +1,31 @@
-vim.opt.autoindent = true
-vim.opt.backup = false
-vim.opt.clipboard = "unnamedplus"
-vim.opt.expandtab = true
-vim.opt.foldmethod = "marker"
-vim.opt.hlsearch = false
-vim.opt.ignorecase = true
-vim.opt.incsearch = true
-vim.opt.modeline = false
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.ruler = true
-vim.opt.scrolloff = 3
-vim.opt.shiftwidth = 2
-vim.opt.showmode = false
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.tabstop = 8
-vim.opt.termguicolors = true
-vim.opt.timeoutlen = 250
-vim.opt.title = true
+
 vim.opt.undofile = true
+vim.opt.updatetime = 500
+vim.opt.timeoutlen = 500
+
 vim.opt.wrap = false
 
+vim.opt.ruler = true
+vim.opt.scrolloff = 3
+vim.opt.showmode = false
+vim.opt.title = true
+
+vim.opt.modeline = false
+
+vim.opt.termguicolors = true
+
 vim.g.neovide_window_blurred = true
-vim.o.guifont = "CommitMonoNoSK:h12"
+vim.o.guifont = "IBM Plex Mono:h12"
