2ad18dc53edf89798d355410934f19bfb9ff88ee
Shreeram Modi <smodi@smodi.net>
2024-08-04 18:20:49 -0700

    nvim: Use smart-splits for window splits
    Replace utils function with smart-splits plugin

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -70,6 +70,87 @@ return {
   },
 
   {
+    "mrjones2014/smart-splits.nvim",
+    opts = {},
+    keys = {
+      {
+        "<M-k>",
+        function()
+          require("smart-splits").resize_up()
+        end,
+      },
+      {
+        "<M-j>",
+        function()
+          require("smart-splits").resize_down()
+        end,
+      },
+      {
+        "<M-h>",
+        function()
+          require("smart-splits").resize_left()
+        end,
+      },
+      {
+        "<M-l>",
+        function()
+          require("smart-splits").resize_right()
+        end,
+      },
+
+      {
+        "<C-k>",
+        function()
+          require("smart-splits").move_cursor_up()
+        end,
+      },
+      {
+        "<C-j>",
+        function()
+          require("smart-splits").move_cursor_down()
+        end,
+      },
+      {
+        "<C-h>",
+        function()
+          require("smart-splits").move_cursor_left()
+        end,
+      },
+      {
+        "<C-l>",
+        function()
+          require("smart-splits").move_cursor_right()
+        end,
+      },
+
+      {
+        "<C-w>K",
+        function()
+          require("smart-splits").swap_buf_up({ move_cursor = true })
+        end,
+      },
+      {
+        "<C-w>J",
+        function()
+          require("smart-splits").swap_buf_down({ move_cursor = true })
+        end,
+      },
+      {
+        "<C-w>H",
+        function()
+          require("smart-splits").swap_buf_left({ move_cursor = true })
+        end,
+      },
+      {
+        "<C-w>L",
+        function()
+          require("smart-splits").swap_buf_right({ move_cursor = true })
+        end,
+      },
+    },
+  },
+
+  {
     "folke/which-key.nvim",
     event = "VeryLazy",
     init = function()
--- .config/nvim/lua/shreeram/keybinds.lua
+++ .config/nvim/lua/shreeram/keybinds.lua
@@ -8,26 +8,6 @@ vim.keymap.set("n", "<Leader>w", vim.cmd.write, { desc = "Write buffer" })
 vim.keymap.set("n", "<Leader>ww", vim.cmd.write, { desc = "Write buffer" })
 vim.keymap.set("n", "<Leader>wq", vim.cmd.wq, { desc = "Write buffer and quit" })
 
-vim.keymap.set({ "n", "x" }, "<C-l>", "<C-w>l", { desc = "Move to right split" })
-vim.keymap.set({ "n", "x" }, "<C-h>", "<C-w>h", { desc = "Move to left split" })
-vim.keymap.set({ "n", "x" }, "<C-j>", "<C-w>j", { desc = "Move to below split" })
-vim.keymap.set({ "n", "x" }, "<C-k>", "<C-w>k", { desc = "Move to above split" })
-
--- TODO Use v:count for counted remappings
-local resize = require("shreeram.utils").resize
-vim.keymap.set({ "n", "x" }, "<M-j>", function()
-  resize(false, 3)
-end)
-vim.keymap.set({ "n", "x" }, "<M-k>", function()
-  resize(false, -3)
-end)
-vim.keymap.set({ "n", "x" }, "<M-l>", function()
-  resize(true, 3)
-end)
-vim.keymap.set({ "n", "x" }, "<M-h>", function()
-  resize(true, -3)
-end)
-
 vim.keymap.set("n", "<leader>co", vim.cmd.copen, { desc = "Open quickfixlist" })
 vim.keymap.set("n", "<leader>cn", vim.cmd.cnext, { desc = "Go to next error" })
 vim.keymap.set("n", "<leader>cp", vim.cmd.cprev, { desc = "Go to prev error" })
